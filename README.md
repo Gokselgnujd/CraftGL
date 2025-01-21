1e645602038b3eb30f5130abff8a0dc50219d4ba
Gokselgnujd <109553608+Gokselgnujd@users.noreply.github.com>
2025-01-21 14:43:03 +0200

    added function to main and create readme.md

---
+++ README.md
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+    <h1>CraftGL introductions</h1>
+    <p>
+        CraftGL is an graphics library made for craft os
+    </p>
+</body>
+</html>
--- main.lua
+++ main.lua
@@ -1,3 +1,18 @@
 local m = {}
 
+function m.createWindow()
+    local window = {}
+    window.Term = term.current()
+end
+
+function m.isTrue(v)
+    if v == true then
+        return true
+    elseif v == false then
+        return false
+    else
+        return "Invalid"
+    end
+end
+
 return m
